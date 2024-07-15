b4b63ac6ff730cb36f25390025456547c0ccdec4
MauroGariazzo <mauromggariazzo@gmail.com>
2024-07-15 22:01:12 +0200

    Update Class Product & Market
    aggiunta classe Product + gestione product all'interno della classe Market

--- src/manager/market/Market.java
+++ src/manager/market/Market.java
@@ -1,10 +1,42 @@
 package manager.market;
+import java.util.ArrayList;
 
-public class Market {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Market {	 
+	private ArrayList<Product> products;
+	
+	public Market(ArrayList<Product> products) {
+		this.products = products;
 	}
-
+	
+	
+	public void addProduct(Product product) {
+		if(!products.contains(product)) {
+			products.add(product);
+		}
+	}
+	
+	
+	public void editProduct(Product newProduct, Product productToSubstitute) 
+	{					
+		if(!products.contains(productToSubstitute)) {
+			System.out.println("Prodotto non trovato");
+			return;
+		}		
+		products.set(productToSubstitute.getId(), newProduct);
+		System.out.println("Prodotto modificato con successo");
+	}
+	
+	public void deleteProduct(Product productToDelete) {
+		if(!products.contains(productToDelete)) {
+			System.out.println("Prodotto non trovato");
+			return;
+		}		
+		products.remove(productToDelete);
+		System.out.println("Prodotto rimosso con successo");
+	}
+	
+	public ArrayList<Product> getAllProducts(){
+		return products;
+	}
+	
 }
--- src/manager/market/Product.java
+++ src/manager/market/Product.java
@@ -1,5 +1,53 @@
 package manager.market;
 
 public class Product {
+	private int id;
+	private String name;
+	private Category category;
+	private double price;
+	
+	public Product(String name, Category category, double price) {
+		id++;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	public Category getCategory() {
+		return category;
+	}
 
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Prodotto{Nome: %s, Categoria: %s, Prezzo: %f }", name,category,price);		
+	}
 }
