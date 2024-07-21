6e761cf0b2719a395c574f581e39eb3d038673c8
Daniel Camuffo <dankemedic@hotmail.com>
2024-07-21 20:52:39 +0200

    updated homepage

--- WebContent/src/homepage.js
+++ WebContent/src/homepage.js
@@ -1,16 +1,23 @@
-/**
- * 
- */
+let mediaWidthMatcher = window.matchMedia("(max-width: 1000px)"); //media matcher for carousel options
+mediaWidthMatcher.addEventListener("change", handlePageSize);
 
+document.getElementById("navbar-ham-btn").addEventListener("mousedown", handleHamburgerMenu); //click on hamburger event
+document.getElementById("carouselTutorial").addEventListener("slid.bs.carousel", event => handleCarouselCaptions(event)); //carousel slide change caption
 
-document.getElementById("navbar-ham-btn").addEventListener("mousedown", handleHamburgerMenu);
+window.addEventListener("load", handlePageSize); //manage on load stuff
 
 screen.orientation.addEventListener("change", () => {
 	
+	handlePageSize();
+	
 	let menu = document.getElementById("hamburger-menu");
 	if(menu != null) {
-		menu.classList.remove("hamburger-menu-open");
-		menu.style.width = 0;
+		let hamburger = document.getElementById("navbar-ham-btn");
+		setTimeout(() => {
+			menu.style.height = 0;
+			menu.classList.remove("hamburger-menu-open");
+			hamburger.style.transform = "rotate(0)";
+		}, 0);
 	}
 });
 
@@ -19,17 +26,60 @@ function handleHamburgerMenu() {
 	const navbarSize = navbar.getBoundingClientRect();
 
 	let menu = document.getElementById("hamburger-menu");
+	let hamburger = document.getElementById("navbar-ham-btn");
+	
+	let size = 0;
+	for(child of menu.children) {
+		let height = parseInt(window.getComputedStyle(child).getPropertyValue("height"));
+		size += height;
+	}
 	if(menu.classList.toggle("hamburger-menu-open")) {
 		setTimeout(() => {
+			hamburger.style.transform = "rotate(90deg)";
 			menu.style.top = navbarSize.height + "px";
-			menu.style.width = "20vw";
+			menu.style.height = size + "px";
 		}, 0);
 	} else {
-		setTimeout(() => {	
-			menu.style.width = 0;
+		setTimeout(() => {
+			hamburger.style.transform = "rotate(0)";
+			menu.style.height = 0;
 		}, 0);
-	}
+	}	
+}
 
-
+function handleCarouselCaptions(event) {
+	let captions = document.querySelectorAll(".caption");
 	
+	if(captions[event.from] != null) {
+		captions[event.from].style.display = "none";
+	}
+	if(captions[event.to] != null) {
+		captions[event.to].style.display = "initial";
+	}
+}
+
+function handlePageSize() {
+	let visibleCarousel = document.querySelector(".carousel-item.active");
+	let captions = document.querySelectorAll(".caption");
+	if(visibleCarousel != null) {
+		visibleCarousel.classList.remove("active");
+	}
+	
+	if(mediaWidthMatcher.matches) {
+		let videoItem = document.getElementById("carousel-video");
+		videoItem.classList.add("active");
+		
+		for(let caption of captions) {
+			caption.style.display = "none";
+		}
+		captions[captions.length - 1].style.display = "initial";
+	} else {
+		let firstItem = document.getElementById("carousel-first-item");
+		firstItem.classList.add("active");
+		
+		for(let caption of captions) {
+			caption.style.display = "none";
+		}
+		captions[0].style.display = "initial";
+	}
 }
