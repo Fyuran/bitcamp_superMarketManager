e9b7d9a81d1ccfe341ceb440136a3cfb2514a673
Daniel Camuffo <dankemedic@hotmail.com>
2024-07-22 17:56:04 +0200

    javadoc comments for Item.java

--- src/manager/market/Item.java
+++ src/manager/market/Item.java
@@ -1,5 +1,14 @@
 package manager.market;
 
+/**
+* Used internally as base class for Product and Offer
+*
+* @author Daniel Camuffo
+* @see Product
+* @see Offer
+* @since 1.2
+*/
+
 public abstract class Item {
 
 }
